cb5ae5e2c2d6820a149ec9b1d0217fbfaa645e81
o0misty0o <o0misty0o@yandex.ru>
2024-05-21 17:27:39 +0300

    qw

---
+++ app/src/main/java/com/example/vkr/posts/PostViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.vkr.posts;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PostViewHolder extends RecyclerView.ViewHolder {
+    public PostViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
